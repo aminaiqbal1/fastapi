89f0b794175affcc7c9b74cd0095864f5d632b19
Amina Iqbal <114309377+aminaiqbal1@users.noreply.github.com>
2025-02-18 09:08:46 +0500

    Add files via upload

---
+++ iamage_uppload.py
@@ -0,0 +1,73 @@
+#upload/main.py
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import os
+from random import randint
+import uuid
+ 
+IMAGEDIR = "images/"
+ 
+app = FastAPI()
+ 
+ 
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+ 
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+ 
+    #save the file
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+ 
+    return {"filename": file.filename}
+ 
+ 
+@app.get("/show/")
+async def read_random_file():
+ 
+    # get random file from the image directory
+    files = os.listdir(IMAGEDIR)
+    random_index = randint(0, len(files) - 1)
+ 
+    path = f"{IMAGEDIR}{files[random_index]}"
+     
+    return FileResponse(path)
+import base64
+from openai import OpenAI
+
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "short-quotes-swl-231117-02-33d404.webp"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpeg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
+)
+
+print(response.choices[0])
